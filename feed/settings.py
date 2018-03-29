4b65a15c722671dc8c82b5f7631fed2c035f4873
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-03-29 15:04:24 +0000

    Procfile

--- feed/settings.py
+++ feed/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 't)rr&_93i#jrpzk%#iawnphv!yroc7b&+ol!-oz4l=$v0i3t$u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["feed-app-wolfenchic.c9users.io"]
+ALLOWED_HOSTS = ["feed-app-wolfenchic.c9users.io", "wolfenchic-feed-fast.herokuapp.com"]
 
 
 # Application definition
@@ -81,11 +82,9 @@ WSGI_APPLICATION = 'feed.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
 }
+
 
 
 # Password validation
